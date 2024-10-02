9dc9be619ebf62ed383beb565d8532f2c22cafe5
Josh Birch <2312292@chester.ac.uk>
2024-10-02 12:25:24 +0100

    Initialise test file
    Written out pseudocode for password generation in test file. Will write code to generate these in the console to get started.

---
+++ tests.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+letters_1 = string.ascii_letters
+digits_2 = string.digits
+symbols_3 = string.punctuation
+whitespace_4 = " "
+
+
+# Generate random number 1-4 (in accordance with list number)
+
+    # If number is same as last number generated, do it again
+    # Else if whitespace_4 used more than 3 times, skip
+    # Else pop (remove and use) random element from list number generated
+    # Add element to generated string whilst string < 18 characters
+
+# When string = 18 characters, print string
+
+# Check against password strength meter?
+
+# Reset lists
+
+
+
+
+
