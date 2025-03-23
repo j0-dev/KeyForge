c910e86e5742a61fc478ac3078ada5b07203e86e
Josh Birch <joshuabirch@protonmail.com>
2025-03-23 07:52:56 +0000

    Remove GUI for CLI-only version
    - Removed Tkinter GUI for future front-end development
    - Renamed "main.py" to "core.py" to contain just the password logic

---
+++ core.py
@@ -0,0 +1,46 @@
+import secrets
+import string
+
+# Password generator function
+def password():
+
+    lowercase_1 = list(string.ascii_lowercase)
+    uppercase_2 = list(string.ascii_uppercase)
+    digits_3 = list(string.digits)
+    symbols_4 = list(string.punctuation)
+
+    password = ""
+
+    while len(password) < 18:
+        list_nums = [1, 2, 3, 4]
+        list_pick = secrets.choice(list_nums)
+
+        if list_pick == 1:
+            char = secrets.choice(lowercase_1)
+            lowercase_1.remove(char)
+            password += char
+
+        elif list_pick == 2:
+            char = secrets.choice(uppercase_2)
+            uppercase_2.remove(char)
+            password += char
+
+        elif list_pick == 3:
+            char = secrets.choice(digits_3)
+            password += char
+
+        else:
+            char = secrets.choice(symbols_4)
+            symbols_4.remove(char)
+            password += char
+
+    return password
+
+print(password())
+
+
+
+
+
+
+    
